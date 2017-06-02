68991daeca662e86a0bcd677408949fe41e3e6d2
FedericoCiancaglini <federico.ciancaglini@ing.austral.edu.ar>
2017-06-01 22:31:07 -0300

    Created a new website for controlling the lights and the alarm

--- photonScript.js
+++ photonScript.js
@@ -2,7 +2,7 @@ var led_on = false;
 
 function postToServer(func, argument){
 //var postUrl = 'https://api.particle.io/v1/devices/28002d000647343339373536/' + func + '?access_token=2b28691b3f97329e5ef8385402bcb972a6d6f592';
-var postUrl = 'http://localhost:8080/v1/devices/28002d000647343339373536/' + func + '?access_token=f928de80111326815054f7ffce386b1dc931533c';
+var postUrl = 'http://192.168.2.2:8080/v1/devices/28002d000647343339373536/' + func + '?access_token=5eefa91a0205cf41e2a132cb0dd7a77d32f390bf';
 $.ajax({
 	url: postUrl,
 	type: 'post',
