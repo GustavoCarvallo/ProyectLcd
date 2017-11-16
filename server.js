e9353dbeafcff9c45afae17182220ff95cdf5f0d
FedericoCiancaglini <federico.ciancaglini@ing.austral.edu.ar>
2017-11-16 19:06:45 -0300

    Added method in server.js to download the log file

--- server.js
+++ server.js
@@ -34,3 +34,8 @@ app.post('/log', function(req, res){
 app.get('/log', function(req, res){
   res.json(log);
 })
+
+app.get('/download', function(req, res){
+  var file = __dirname + 'log.txt';
+  res.download(file); // Set disposition and send it.
+});
